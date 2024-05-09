f69c469e513b268eb0a4a68e084f65b6908d4530
Gouthamkumar025 <gouthamkumar.s.2021.csbs@ritchennai.edu.in>
2024-05-09 18:47:31 +0530

    first commit

---
+++ lib/questions.dart
@@ -0,0 +1,9 @@
+class Questions {
+  String questionText;
+  bool questionanswer;
+
+  // Constructor with named parameters
+  Questions(
+      this.questionText, this.questionanswer); 
+      // Initialize questionanswer here
+}
